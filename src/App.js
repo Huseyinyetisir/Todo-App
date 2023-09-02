5aa307782a5b9a2d4eba001e366c59e7106ad366
Hüseyin Yetişir <huseynyetisir@gmail.com>
2023-09-03 01:17:12 +0300

    Add Todo function added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import React, {useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
 
-  const todos = [
+  const [todos, setTodos] = useState ([
     {rowNumber:1, rowDescription: 'Feed puppy', rowAssigned: 'Sadik'},
     {rowNumber:2, rowDescription: 'Water plants', rowAssigned: 'Okan'},
     {rowNumber:3, rowDescription: 'Make dinner', rowAssigned: 'Sadik'},
     {rowNumber:4, rowDescription: 'Feed puppy', rowAssigned: 'Sadik'},
-    {rowNumber:5, rowDescription: 'Water plants', rowAssigned: 'Okan'},
+    {rowNumber:5, rowDescription: 'Moved tickets', rowAssigned: 'Huseyin'},
     {rowNumber:6, rowDescription: 'Make dinner', rowAssigned: 'Sadik'}
-  ]
+  ])
+
+  const addTodo = (description,assigned) => {
+    const newTodo = {
+      rowNumber: todos.length + 1,
+      rowDescription: description,
+      rowAssigned: assigned
+    };
+    setTodos(todos => [...todos, newTodo])
+  }
   return (
     <div className='mt-5 container'>
       <div className='card'>
@@ -19,6 +30,7 @@ function App() {
         </div>
         <div className='card-body'>
           <TodoTable todos = {todos}/>
+          <NewTodoForm addTodo= {addTodo}/>
         </div>
       </div>
     </div>
